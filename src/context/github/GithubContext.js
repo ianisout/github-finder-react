1b56f9b32d40eacc906f1e63b371a698c3d0170c
unknown <lsouza.ian@gmail.com>
2022-08-04 11:12:41 -0300

    clear search

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -34,13 +34,13 @@ export const GithubProvider = ({ children }) => {
   const searchUsers = async (text) => {
     setLoading();
 
-    const params = new URLSearchParams({ q: text })
+    const params = new URLSearchParams({ q: text });
 
     const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       // headers: {
       //   Authorization: `token ${GITHUB_TOKEN}`
       // }
-    })
+    });
 
     const { items } = await res.json();
 
@@ -52,12 +52,15 @@ export const GithubProvider = ({ children }) => {
 
   const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
+  const clearSearch = () => dispatch({ type: 'CLEAR_SEARCH' });
+
   return (
     <GithubContext.Provider
       value={{
         users: state.users,
         loading: state.loading,
         searchUsers,
+        clearSearch,
       }}
     >
       {children}
